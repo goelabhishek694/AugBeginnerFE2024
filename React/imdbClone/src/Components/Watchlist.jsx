6a0c1c2e4ab2afefa00ae8ceb25cfb11df20a4ee
Abhishek <abhishekgoel@gofynd.com>
2024-09-18 23:38:30 +0530

    watchlist features

--- React/imdbClone/src/Components/Watchlist.jsx
+++ React/imdbClone/src/Components/Watchlist.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
-
+import genreids from "../../utils";
 function Watchlist() {
   const [watchlist, setWatchlist] = useState([]);
-
+  const [search, setSearch] = useState("");
+  const [genre, setGenre] = useState([]);
   useEffect(() => {
     if (localStorage.getItem("watchlist")) {
       let updatedWatchList = JSON.parse(localStorage.getItem("watchlist"));
@@ -10,15 +11,60 @@ function Watchlist() {
       setWatchlist(updatedWatchList);
     }
   }, []);
+
+  const handleIncreasingSorting = () => {
+    console.log("sort increasing on basis of rating");
+    let sortedArr=watchlist.sort((m1,m2) => m1.vote_average - m2.vote_average);
+    console.log(sortedArr);
+    setWatchlist([...sortedArr]);
+  }
+
+  const handleDecreasingSorting = () => {
+    console.log("sort decreasing on basis of rating");
+    let sortedArr=watchlist.sort((m1,m2) => m2.vote_average - m1.vote_average);
+    setWatchlist([...sortedArr]);
+    
+  }
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  }
+
+  useEffect(() => {
+    let allGenres = watchlist.map((movieObj) => genreids[movieObj.genre_ids[0]]);
+    console.log(allGenres);
+    let uniqueGenres = new Set(allGenres);
+    console.log(uniqueGenres);
+    setGenre([...uniqueGenres]);
+  },[watchlist])
+
   return (
+    <>
+    <div className="flex flex-wrap justify-around ">
+      <button className="border border-gray-200 bg-blue-300 m-4 py-3 px-6 rounded-xl">All Genres</button>
+      {
+        genre.map(genreName=>(<button className="border border-gray-200 bg-blue-300 m-4 py-3 px-6 rounded-xl">{genreName}</button>))
+      }
+    </div>
+    <div>
+      <input 
+      type="text" 
+      placeholder="Search Movies"
+      onChange={handleSearch}
+      value={search}
+      className="h-[3rem] w-[18rem] px-4 outline-none border border-slate-600"
+      />
+    </div>
     <div className="overflow-hidden rounded-lg border border-gray-200 shadow-md m-4">
       <table className="w-full border-collapse bg-white text-left text-sm text-gray-500">
         <thead>
           <tr>
             <th>Name</th>
             <th>
-              <div>
-                <div className="flex">Ratings</div>
+              <div className="flex gap-2">
+              <i onClick={handleIncreasingSorting} class="fa-solid fa-arrow-up"></i>
+                <div >Ratings</div>
+                <i onClick={handleDecreasingSorting} class="fa-solid fa-arrow-down"></i>
               </div>
             </th>
             <th>
@@ -34,7 +80,15 @@ function Watchlist() {
           </tr>
         </thead>
         <tbody className="divide-y divide gray-100 border-t border-gray-100">
-          {watchlist.map((movieObj) => {
+          {watchlist
+          // this function is sued to filter watchlist on the basis of search 
+          .filter((movieObj)=> {
+            let searchText = search.toLowerCase();
+            let movieName = movieObj.title.toLowerCase();
+            return movieName.includes(searchText);
+          })
+          // this function is used to display watchlist on UI
+          .map((movieObj) => (
             <tr className="hover:bg-gray-50">
               <td className="flex items-center px-6 py-4 font-normal text-gray-900">
                 <img
@@ -48,12 +102,13 @@ function Watchlist() {
               </td>
               <td className='className="pl-6 py-4"'>{movieObj.vote_average}</td>
               <td className="pl-6 py-4">{movieObj.popularity}</td>
-              <td className="pl-6 py-4">Action</td>
-            </tr>;
-          })}
+              <td className="pl-6 py-4">{genreids[movieObj.genre_ids[0]]}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
+    </>
   );
 }
 
