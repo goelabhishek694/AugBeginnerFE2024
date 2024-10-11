bbcd3f32b40fbce4e13a4809f1c971c6df991522
Abhishek <abhishekgoel@gofynd.com>
2024-10-11 22:04:36 +0530

    unit test cases

---
+++ React/testing/src/Components/Counter.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+
+function Counter() {
+    const [count, setCount] = useState(0);
+    const incCount = () => {
+        setCount(count+1);
+    }
+    const decCount = () => {
+        setCount(count-1);
+    }
+  return (
+    <div>
+        <button onClick={incCount}>+</button>
+        <h2>{count}</h2>
+        <button onClick={decCount}>-</button>
+    </div>
+  )
+}
+
+export default Counter
